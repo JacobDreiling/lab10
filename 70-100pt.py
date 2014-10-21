5a7b2daf0c9bcd5b70e8516680eed29dcf678da3
JacobDreiling <quasarbooster@gmail.com>
2014-10-21 16:05:57 -0500

    70pt

--- 70-100pt.py
+++ 70-100pt.py
@@ -24,5 +24,17 @@ drawpad.grid(row=0, column=1)
 
 # Insert your code here to draw the house!
 
+#70pt
+#rek'd's
+forwaertshaus = drawpad.create_rectangle(160,350,400,540, fill = 'purple') #100pt- including fills now
+rueckwaertshaus = drawpad.create_rectangle(400,390,640,540, fill = 'purple')
+#lines -painful..
+unterline = drawpad.create_line(80,350,480,350, fill = 'gray')
+ueberline = drawpad.create_line(140,300,420,300, fill = 'gray')
+linkeline = drawpad.create_line(80,350,140,300, fill = 'gray')
+rechtline = drawpad.create_line(420,300,480,350, fill = 'gray')
+cylrecht = drawpad.create_line(640,240,640,540, fill = 'lightblue')
+cylueber = drawpad.create_line(400,240,640,240, fill = 'lightblue')
+cyltopl = drawpad.create_line(400,240,400,300, fill = 'lightblue')
 
 root.mainloop()
